f4d6418fe2061b0dd94eb91df1fa51bb30da296f
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2015-10-02 11:53:10 -0400

    Change to use python 2 since that's already in the container

--- workflow/bin/remove_bad_repo.py
+++ workflow/bin/remove_bad_repo.py
@@ -1,22 +1,22 @@
-#! /usr/bin/python3
+#! /usr/bin/python
 
 import json
 import sys
 import re
-import urllib
-import urllib.request
+import urllib2
+#import urllib.request
 
 
 # TODO: Parameterize the URL. Not that it's likely to change over the course of this production run, it's just nicer that way. ;)
 url ='https://raw.githubusercontent.com/ICGC-TCGA-PanCancer/pcawg-operations/develop/variant_calling/broad_workflow/blacklists/blacklist_repo.txt'
-blacklist_file = urllib.request.urlopen(url).read().decode('UTF-8')
+blacklist_file = urllib2.urlopen(url).read().decode('UTF-8')
 print('raw blacklist file: '+blacklist_file)
 lines = blacklist_file.split('\n')
 
 bad_repos = ''
 
 for line in lines:
-    if not line.startswith('#'):
+    if not line.startswith('#') and line != '':
         bad_repos = bad_repos + 'gtrepo-'+line+'.annailabs.com' + ','
 
 print('repo list is: '+bad_repos)
@@ -34,11 +34,12 @@ if bad_repos != '':
         # now remove the bad repo from tumour and normal
         # the regular expression is to clean up extra commas at the begining or end of string, or any multiple-commas created by removing the bad repo name
         for repo in bad_repos.split(','):
-            json_data["parameters"]["tumor_bam_download"]["gnos_endpoint"] = re.sub(r'^,|,{2,}|,$','', json_data["parameters"]["tumor_bam_download"]["gnos_endpoint"].replace(repo,'')  )
-            json_data["parameters"]["normal_bam_download"]["gnos_endpoint"]=  re.sub(r'^,|,{2,}|,$','', json_data["parameters"]["normal_bam_download"]["gnos_endpoint"].replace(repo,'') )
-            print('\nafter cleaning out \''+repo+'\': ')
-            print('tumour endpoint: '+json_data["parameters"]["tumor_bam_download"]["gnos_endpoint"])
-            print('normal endpoint: '+json_data["parameters"]["normal_bam_download"]["gnos_endpoint"])
+            if repo != '':
+                json_data["parameters"]["tumor_bam_download"]["gnos_endpoint"] = re.sub(r'^,|,{2,}|,$','', json_data["parameters"]["tumor_bam_download"]["gnos_endpoint"].replace(repo,'')  )
+                json_data["parameters"]["normal_bam_download"]["gnos_endpoint"]=  re.sub(r'^,|,{2,}|,$','', json_data["parameters"]["normal_bam_download"]["gnos_endpoint"].replace(repo,'') )
+                print('\nafter cleaning out \''+repo+'\': ')
+                print('tumour endpoint: '+json_data["parameters"]["tumor_bam_download"]["gnos_endpoint"])
+                print('normal endpoint: '+json_data["parameters"]["normal_bam_download"]["gnos_endpoint"])
 
     # Exit with error code 1 if either endpoint is null. A null endpoint *after* removing blacklisted repos means that data to download is *only* available
     # on a blacklisted repo and if it's blacklisted, we don't want to download from there.
